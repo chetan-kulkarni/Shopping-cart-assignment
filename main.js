7f71e5b4566ae50cb4c42bd7194995b036ebe631
chetan <cdkulkarni123@gmail.com>
2020-11-05 14:37:42 +0530

    checkout process

--- main.js
+++ main.js
@@ -20,7 +20,7 @@
             <img class="shop-item-image" src="${items.image}">
                 <div class="shop-item-details">
                     <span class="shop-item-title">${items.name}</span>
-                    <span class="shop-item-price strike-out">$${items.price.actual}</span>
+                    <span class="shop-item-original-price strike-out">$${items.price.actual}</span>
                     <span class="shop-item-price totalPrice"><strong> $${totalPrice}</strong></span>
                     <button onclick="addToCartClicked()" class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
                 </div>
@@ -36,9 +36,10 @@
         var button = event.target
     var shopItem = button.parentElement.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var actualPrice = shopItem.getElementsByClassName('shop-item-original-price')[0].innerText
     var price = shopItem.getElementsByClassName('totalPrice')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
+    addItemToCart(title, price, imageSrc,actualPrice)
     updateCartTotal()
     }
     
@@ -48,7 +49,7 @@
         updateCartTotal()
     }
 
-    function addItemToCart(title, price, imageSrc) {
+    function addItemToCart(title, price, imageSrc, actualPrice) {
         var cartRow = document.createElement('div')
         cartRow.classList.add('cart-row')
         var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -68,7 +69,8 @@
             <div class="cart-quantity cart-column">
                 <input class="cart-quantity-input" type="number" value="1" style="width:50px">
             </div>
-            <span class="cart-price cart-column">${price}</span>`
+            <span class="cart-price cart-column">${price}</span>
+            <span class="actualPrice hidden">${actualPrice}`
         cartRow.innerHTML = cartRowContents
         cartItems.append(cartRow)
         cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
@@ -85,17 +87,28 @@
     function updateCartTotal() {
         var cartItemContainer = document.getElementsByClassName('cart-items')[0]
         var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        var totalPrice = 0
         var total = 0
+        var orderTotal = 0
         for (var i = 0; i < cartRows.length; i++) {
             var cartRow = cartRows[i]
             var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+            var discountElement = cartRow.getElementsByClassName('actualPrice')[0]
             var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
             var price = parseFloat(priceElement.innerText.replace('$', ''))
+            var discout = parseFloat(discountElement.innerText.replace('$', ''))
+            console.log(price)
             var quantity = quantityElement.value
             total = total + (price * quantity)
+            totalPrice = totalPrice + (discout * quantity)
+            discount = totalPrice - total
+            orderTotal = total
         }
         total = Math.round(total * 100) / 100
-        document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+        document.getElementsByClassName('cart-total-price')[0].innerText = '$' + totalPrice
+        document.getElementsByClassName('cart-total-discout')[0].innerText = '$' + discount
+        document.getElementsByClassName('cart-order-total')[0].innerText = '$' + orderTotal
+        
     }
 
     
