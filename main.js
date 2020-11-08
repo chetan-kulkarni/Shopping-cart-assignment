4fd9e81a280820812b42e41cb32c1ded97459844
chetan <cdkulkarni123@gmail.com>
2020-11-08 23:44:35 +0530

    some fine tuning

--- main.js
+++ main.js
@@ -5,39 +5,43 @@
         button.addEventListener('click', addToCartClicked)
     }
 
+    /*** Fetch data from JSON ***/
+
     fetch("./product.json")
     .then((res) => res.json())
     .then((data) => {
-        let output = '';
+        let productsList = '';
         data.items.forEach(function(items){
         var actualPrice = Number(items.price.actual);
         var discoutValue = Number(items.discount)/100;
         var afterDiscount = actualPrice - (actualPrice * discoutValue);
         var totalPrice = afterDiscount.toFixed(2);
-        output += `
-        <div class="box">
-        <span class="offer-tag">${items.discount}%</span>
-            <img class="shop-item-image" src="${items.image}">
-            <div class="shop-item-details">
-                <span class="shop-item-title">${items.name}</span><br>
-                <span class="shop-item-original-price strike-out">$${items.price.actual}</span>
-                <span class="shop-item-price totalPrice"><strong> $${totalPrice}</strong></span>
-                <button onclick="addToCartClicked()" class="btn btn-primary shop-item-button" type="button">Add to cart</button>
+        productsList += `
+        <div class="item">
+            <span class="item__offer-tag">${items.discount}%</span>
+            <img class="item__img" src="${items.image}">
+            <div class="item__desc">
+                <h3 class="item__title">${items.name}</h3>
+                <span class="item__actualPrice strike-out">$${items.price.actual}</span>
+                <span class="item__totalPrice"><strong> $${totalPrice}</strong></span>
+                <button onclick="addToCartClicked()" class="btn btn-primary item__button" type="button">Add to cart</button>
             </div>
         </div>
         `;
       });
-      document.getElementById('output').innerHTML = output;
+      document.getElementById('productsList').innerHTML = productsList;
 
     })
+
+    /*** Add to cart Functionality ***/
 
     function addToCartClicked() {
         var button = event.target
         var shopItem = button.parentElement.parentElement
-        var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-        var actualPrice = shopItem.getElementsByClassName('shop-item-original-price')[0].innerText
-        var price = shopItem.getElementsByClassName('totalPrice')[0].innerText
-        var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+        var title = shopItem.getElementsByClassName('item__title')[0].innerText
+        var actualPrice = shopItem.getElementsByClassName('item__actualPrice')[0].innerText
+        var price = shopItem.getElementsByClassName('item__totalPrice')[0].innerText
+        var imageSrc = shopItem.getElementsByClassName('item__img')[0].src
         addItemToCart(title, price, imageSrc,actualPrice)
         updateCartTotal()
     }
@@ -48,10 +52,13 @@
         updateCartTotal()
     }
 
+
+    /***  Checkout functionality  ***/
+
     function addItemToCart(title, price, imageSrc, actualPrice) {
         var cartRow = document.createElement('div')
-        cartRow.classList.add('cart-row')
-        var cartItems = document.getElementsByClassName('cart-items')[0]
+        cartRow.classList.add('cart-items')
+        var cartItems = document.getElementsByClassName('cart-details')[0]
         var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
         for (var i = 0; i < cartItemNames.length; i++) {
             if (cartItemNames[i].innerText == title) {
@@ -60,10 +67,10 @@
             }
         }
         var cartRowContents = `
-            <div class="cart-item cart-column box">
-                <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-                <span class="cart-item-title">${title}</span>
-                <button class="btn btn-danger" type="button">X</button>
+            <div class="cart-items-details">
+                <img class="cart-items-details__img" src="${imageSrc}" width="100" height="100">
+                <span class="cart-items_details__title">${title}</span>
+                <button class="cart-items-details__button" type="button">X</button>
             </div>
             <div class="cart-quantity cart-column">
                 <input class="cart-quantity-input" type="number" value="1" style="width:50px">
@@ -72,7 +79,7 @@
             <span class="actualPrice hidden">${actualPrice}`
         cartRow.innerHTML = cartRowContents
         cartItems.append(cartRow)
-        cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+        cartRow.getElementsByClassName('cart-items-details__button')[0].addEventListener('click', removeCartItem)
         cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
     }
     function quantityChanged(event) {
@@ -84,8 +91,8 @@
     }
     
     function updateCartTotal() {
-        var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-        var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        var cartItemContainer = document.getElementsByClassName('cart-details')[0]
+        var cartRows = cartItemContainer.getElementsByClassName('cart-items')
         var totalPrice = 0
         var total = 0
         var orderTotal = 0
@@ -100,13 +107,13 @@
             var quantity = quantityElement.value
             total = total + (price * quantity)
             totalPrice = totalPrice + (discout * quantity)
-            discount = totalPrice - total
+            discount = (totalPrice - total).toFixed(2)
             orderTotal = total
         }
         total = Math.round(total * 100) / 100
         document.getElementsByClassName('cart-total-price')[0].innerText = '$' + totalPrice
         document.getElementsByClassName('cart-total-discout')[0].innerText = '$' + discount
-        document.getElementsByClassName('cart-order-total')[0].innerText = '$' + orderTotal
+        document.getElementsByClassName('order-details__cart-order-total')[0].innerText = '$' + orderTotal
         
     }
 
